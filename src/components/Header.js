14719872f3b607e68eee3c1e28b2f24a5bc91704
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-28 23:23:13 +0900

    update project

---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+//import Library
+import React from "react"
+import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+//import Actions
+
+
+//import elements
+import {  Grid, Input, Image, Text } from "../elements" 
+
+//import Mui
+import Avatar from '@mui/material/Avatar';
+import { Modal } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Snackbar from '@mui/material/Snackbar';
+
+//import Icon
+import HomeIcon from '@mui/icons-material/Home';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import GroupRoundedIcon from '@mui/icons-material/GroupRounded';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import LoginIcon from '@mui/icons-material/Login';
+
+
+
+// impot Component
+
+
+//import Actions
+
+
+
+
+const Header = (props) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+    React.useEffect(async() => {
+
+    },[]);
+    
+    return(
+            <Grid>
+                <Grid position="absolute" top="0px" is_flex align-items="center" justify-content='space-between' box-sizing="border-box" padding="0" width ="320px" height='60px' margin='0'  >
+                    <div onClick={()=>{navigate('/')}}>
+                        <img width={"50px"} alt="instagram letter Logo" src="/Logo/soso.jpeg"/>
+                    </div>
+                    <Grid is_flex border="0">
+                        <Tooltip title="Add Post"><IconButton sx={{width:"50px", height : "50px"}}><FavoriteBorderOutlinedIcon  sx={{ margin :"10px"}}/></IconButton></Tooltip>    
+                    </Grid>
+                </Grid>
+                <Grid margin="0 0 50px 0"/>
+            </Grid>
+
+    );
+
+}
+
+
+export default Header;
--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -12,7 +12,7 @@ const Grid = ({
     console.log(props);
     if(wrap){
         return(
-            <Wrap style={{display:"flex",justifyContent:"center",}}>
+            <Wrap style={{display:"flex"}}>
                 <div onClick={onClick} style={{...props}}>
                     {children}
                 </div>
@@ -38,20 +38,21 @@ const Wrap = styled.div`
     width : ${props => props.theme.size.width};
     height : ${props => props.theme.size.height};
     background-color : ${props => props.theme.mainTheme.primary};
+    overflow : hidden;
 
 `;
 
 const Div = styled.div`
     background-color : ${props => props.theme.mainTheme.primary};
     border : 1px solid ${props => props.theme.mainTheme.secondary};
-
+    box-sizing : border-box;
 `;
 
 const Flex = styled.div`
     display : flex;
     background-color : ${props => props.theme.mainTheme.primary};
     border : 1px solid ${props => props.theme.mainTheme.secondary};
-
+    box-sizing : border-box;
 `;
 
 
