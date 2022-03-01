30eff732d41501405c2ae273dd9775da2af5af4c
sojh93 <36619557+sojh93@users.noreply.github.com>
2022-03-01 17:16:31 +0900

    계속 안됨 어휴
    닉넴이 프로필사진 옆으로 안감

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -10,6 +10,9 @@ import { useNavigate } from "react-router-dom";
 import { Button, Grid, Input, Image, Text } from "../elements";
 
 //import Icon
+import { AiOutlineHeart } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import SwitchSelector from "react-switch-selector";
 
 // impot Component
 import Header from "../components/Header";
@@ -17,11 +20,8 @@ import Header from "../components/Header";
 //import Actions
 //import axios
 import instance from "../shared/Request";
-// import Header from "../components/Header";
 import HorizonLine from "../components/HorizonLine";
-import { AiOutlineHeart } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import SwitchSelector from "react-switch-selector";
+
 
 const Mypage = () => {
 
@@ -49,39 +49,44 @@ const Mypage = () => {
   return (
     <Grid wrap height="100%">
       <Header/>
-      <Grid is_flex flexDirection="column" height="calc(30% - 121px)" is_scroll padding="0">
-        <Grid is_flex flexDirection="row" margin="5px 5px 0 0">
-          <CgProfile size="100" style={{ width: "30%" }} padding="0" margin="0"/>
+      <Grid wrap  padding="0" width="100%">
+        <Grid height="110px" flexDirection="row" margin="5px 5px 0 0" width="100%">
+          <CgProfile size="90" style={{ width: "40%" }} padding="0" margin="15px 0 0 0"/>
           <Grid
             is_flex
             height="30px"
-            margin="0 0 0 10px"
-            width="43%"
+            margin="5px 0 0 0"
+            width="80px"
             flexDirection="row"
           >
             닉네임
           </Grid>
           <Grid
-            is_flex
-            height="55px"
-            textAlign="center"
-            margin="18px 0 0 35px"
-            width="40%"
-            flexDirection="row"
-            padding="0"
-          >
-            프로필 수정하기
-          </Grid>
-        </Grid>
-        <Grid is_flex flexDirection="column" width="100%">
-            <Grid
               is_flex
               height="30px"
-              margin="0 5px 0px 80px"
-              flexDirection="column"
+              margin="40px 0 0 0"
+              width="20%"
+              
+
             >
               소개
             </Grid>
+            <Grid
+            is_flex
+            height="55px"
+            textAlign="center"
+            margin="15px 0 0 0"
+            width="25%"
+            flexDirection="row"
+            padding="0"
+          >
+          <Button>프로필 수정하기</Button>
+          </Grid>
+        </Grid>
+       
+        
+        <Grid is_flex flexDirection="column" width="100%">
+            
           <Grid
             is_flex
             height="30px"
