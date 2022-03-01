e4ad752e59106cca270f06ecd9e780583b658bce
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-03-01 16:54:02 +0900

    Update PostList.js

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -3,6 +3,7 @@ import React from "react"
 import styled from "styled-components";
 import {useDispatch, useSelector} from "react-redux";
 import { useNavigate } from "react-router-dom";
+import SwitchSelector from "react-switch-selector";
 
 //import Actions
 
@@ -24,9 +25,35 @@ function PostList() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
+
+    const options = [
+        {
+            label: "추천",
+            value: "recommend",
+            width : "100px",
+        },
+        {
+            label: "최신",
+            value: "recent",
+            width : "100px",
+        }
+    ];
+    const onChange = (newValue) => {
+        console.log(newValue);
+    };
+    
+    const initialSelectedIndex = options.findIndex(({value}) => value === "recommend");
+
     return (
         <Grid wrap>
             <Header/>
+
+            <Grid margin="15px" width="100px" height="30px">
+                <SwitchSelector
+                    onChange={onChange}
+                    options={options}
+                />
+            </Grid>
             <Bottom thisPage="postList"/>
         </Grid>
 
