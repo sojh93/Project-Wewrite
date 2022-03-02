763024f542d201938de421eeffbbf00f499ff6a1
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-03-02 20:15:50 +0900

    update redux user data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,11 +5,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 //import Modules
 import Map from "./modules/map";
+import User from './modules/user';
 
 
 const middlewares = [thunk];
 const rootReducer = combineReducers({
     map : Map,
+    user : User,
 });
 const enhancer = applyMiddleware(...middlewares);
 const store = createStore(rootReducer,enhancer);
