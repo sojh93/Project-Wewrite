8cede60841c9441273bfe645e6bd1366a42e51d1
sojh93 <36619557+sojh93@users.noreply.github.com>
2022-03-05 11:39:21 +0900

    0305 #1 first commit
    의미 없는 커밋

---
+++ src/components/BooksPrac.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// import elements
+import { Grid, Image } from "../elements";
+
+// import Components
+
+// import default image
+import book1 from "../image/SleepingDog.jpg";
+import book2 from "../image/manTrainingCat.png";
+import book3 from "../image/BeingExistAndTime.jpg";
+import book4 from "../image/KingMakingTheory.jpg";
+import book5 from "../image/Ajeossi.png";
+
+const BooksPrac = (props) => {
+    const books = BooksPrac.map((book, index) => <Grid key={index}>{props.book[index]}</Grid>)
+    console.log(books)
+    return(
+    <>
+
+        <Grid is_flex justifyContent="space-around" width="100%" margin="30px 0 0 0" height="75px" borderBottom="1.5px solid black">
+            <Grid is_flex height="70px" width="20%">
+                {props.books[0]?<Image src={props.books[0]} width="70px" height="70px"/>:""}
+            </Grid>
+            <Grid is_flex height="70px" width="20%">
+                {props.book[1]?<Image src={props.book[1]} width="70px" height="70px"/>:""}
+            </Grid>
+            <Grid is_flex height="70px" width="20%">    
+                {props.book[2]?<Image src={props.book[2]} width="70px" height="70px"/>:""}
+            </Grid>
+        </Grid>
+    </>
+    )
+}
+
+export default BooksPrac;
