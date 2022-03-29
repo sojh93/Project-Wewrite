209e61528caa1650d0a034b48ea6b3de8a0bcab6
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-03-29 16:17:18 +0900

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -63,10 +63,10 @@ const Post = React.memo((props) => {
         <Grid is_flex width='100%' flexDirection='column'>
             <Grid is_flex justifyContent='space-between' width="100%" gap='10px'>
                 <Grid onClick={navigatePost} width='80px' is_flex flexDirection='column'>
-                    <Image boxSizing='border-box' border='1px solid #e0e0e0' width="130px" height="150px" borderRadius="5px" src={props.url?props.url:''}/>
+                    <Image cursor='pointer' boxSizing='border-box' border='1px solid #e0e0e0' width="130px" height="150px" borderRadius="5px" src={props.url?props.url:''}/>
                 </Grid>
                 <Grid is_flex flexDirection="column" width="calc(100% - 130px)" justifyContent='flex-start'>
-                    <Text onClick={navigatePost} margin='0 10px' fontSize="16px" fontWeight='700'>{props.title?props.title:''}</Text>
+                    <Text cursor='pointer' onClick={navigatePost} margin='0 10px' fontSize="16px" fontWeight='700'>{props.title?props.title:''}</Text>
                     <Grid margin='5px 10px' is_flex flexDirection='column' height='50px' justifyContent='space-between'>
                         <Grid>
                             {props.category?props.category.map((v,i)=>{
@@ -74,14 +74,14 @@ const Post = React.memo((props) => {
                             }):''}
                         </Grid>
                         <Grid is_flex fontSize='15px' color='#7E7E7E' fontWeight='300'>
-                            <Image onClick={likePost} width='20px' height='20px' margin='4px' src={props.isLike?'/Icon/thumbs-up-filled.png':'/Icon/thumbs-up.png'}/>   
+                            <Image cursor='pointer' onClick={likePost} width='20px' height='20px' margin='4px' src={props.isLike?'/Icon/thumbs-up-filled.png':'/Icon/thumbs-up.png'}/>   
                             <Text>{props.like?props.like:'0'}</Text>
-                            <Image width='14px' onClick={markPost} height='18px' margin='6px' src={props.isMark?'/Icon/bookmark_filled.png':'/Icon/bookmark.png'}/>
+                            <Image cursor='pointer' width='14px' onClick={markPost} height='18px' margin='6px' src={props.isMark?'/Icon/bookmark_filled.png':'/Icon/bookmark.png'}/>
                             <Text>{props.bookmarkLikesCnt}</Text>
                         </Grid>
                     </Grid>
                     <Grid onClick={navigatePost} margin='0 10px'>
-                        <Text height='70px' fontSize="12px" margin="0px">
+                        <Text cursor='pointer' height='70px' fontSize="12px" margin="0px">
                             {props.first?charLimit(props.first,60):''}
                         </Text>
                     </Grid>
