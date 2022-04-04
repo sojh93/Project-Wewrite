aa5d5dfa0f96fc25e9bedd9eae8ee062c941f727
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-04-04 23:26:02 +0900

    Update PostDetail.js

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -219,10 +219,8 @@ function PostDetail(props) {
                         }
                         if(data.body.split(',')[0].split('\"')[3] === 'QUIT'){
                             console.log('QUIT');
-                            console.log(_post,data.body.split(',')[1].split('\"')[3])
-                            if(thisPost.writer === data.body.split(',')[1].split('\"')[3]){
-                                setTimeout(()=>{window.location.reload()},500)
-                            }
+                            setTimeout(()=>{window.location.reload()},500)
+                            
                         }
                     },
                     headers
